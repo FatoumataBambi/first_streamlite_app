2e579c3c2a1a68a28e5dd104aed6384ceece1000
FatoumataBambi <95967921+FatoumataBambi@users.noreply.github.com>
2022-10-20 10:12:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@ import streamlit
 
 streamlit.title('My parents new heathly diner')
 
+
 streamlit.header('Breakfast Menu')
- 
-
-   
-
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
