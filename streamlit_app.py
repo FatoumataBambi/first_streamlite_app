bf61b3d3b02875728ff3ad0b09a437c25c61ab0a
FatoumataBambi <95967921+FatoumataBambi@users.noreply.github.com>
2022-10-21 11:18:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,15 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+
 #section to display the fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ""kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # recupere les données en format Json depuis le API renseigné les normalise  et les affiche en dataframe  
